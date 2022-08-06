c3c893f4eea758910da0600b4751b9f770da1d14
Richa-iitr <richa@cs.iitr.ac.in>
2022-08-06 10:41:40 +0530

    commit

--- src/insta-automation.ts
+++ src/insta-automation.ts
@@ -1,16 +1,26 @@
-import { Address, BigInt, Bytes, dataSource, log } from "@graphprotocol/graph-ts";
+import {
+  Address,
+  BigInt,
+  Bytes,
+  dataSource,
+  log,
+} from "@graphprotocol/graph-ts";
 import {
   InstaAutomation,
   LogCancelledAutomation,
+  LogChangedOwner,
   LogExecutedAutomation,
-  LogExecutedAutomationParams,
+  LogFeeTransferred,
   LogFlippedExecutors,
   LogSubmittedAutomation,
+  LogSystemCall,
   LogSystemCancelledAutomation,
-} from "../generated/InstaIndex/InstaAutomation";
+  LogUpdatedAutomationFee,
+  LogUpdatedBufferHf,
+  LogUpdatedMinHf,
+} from "../generated/InstaAutomation/InstaAutomation";
 import {
   ExecuteData,
-  ExecuteMetaData,
   ExecutionParams,
   Spell,
   Swap,
@@ -18,22 +28,28 @@ import {
   SubmitData,
   CancelData,
   SystemCancelData,
-  Executor
+  Executor,
 } from "../generated/schema";
 import {
   createOrLoadCancelData,
+  createOrLoadChangedOwner,
   createOrLoadDsa,
   createOrLoadExecute,
-  createOrLoadExecuteMetaData,
   createOrLoadExecutionParams,
-  createOrLoadSpell,
+  createOrLoadFeeTransferData,
   createOrLoadSubmit,
   createOrLoadSwap,
+  createOrLoadSystemCallData,
   createOrLoadSystemCancelData,
+  createOrLoadTransaction,
+  createOrUpdateAutomationFeeData,
+  createOrUpdateBufferHfData,
+  createOrUpdateMinHfData,
 } from "./insta-index";
 
 export function handleLogSubmitAutomation(event: LogSubmittedAutomation): void {
-  let dsaId = event.params.user.toHexString() + "#" + event.params.id.toString();
+  let dsaId =
+    event.params.user.toHexString() + "#" + event.params.id.toString();
   let eventId =
     event.transaction.hash.toHexString() + event.logIndex.toString();
 
@@ -44,6 +60,7 @@ export function handleLogSubmitAutomation(event: LogSubmittedAutomation): void {
   log.info("ID: {}", [dsaId]);
 
   let dsa = createOrLoadDsa(dsaId);
+  dsa.user = event.params.user;
   let submitData = createOrLoadSubmit(eventId);
   submitData.user = event.params.user;
   submitData.userId = event.params.id;
@@ -51,13 +68,22 @@ export function handleLogSubmitAutomation(event: LogSubmittedAutomation): void {
   submitData.thresholdHF = event.params.thresholdHF;
   submitData.currentHf = event.params.currentHf;
   submitData.account = dsaId;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  submitData.transactionDetail = transaction.id;
 
+  transaction.save();
   submitData.save();
   dsa.save();
 }
 
 export function handleLogCancelAutomation(event: LogCancelledAutomation): void {
-  let dsaId = event.params.user.toHexString() + "#" + event.params.id.toString();
+  let dsaId =
+    event.params.user.toHexString() + "#" + event.params.id.toString();
   let eventId =
     event.transaction.hash.toHexString() + event.logIndex.toString();
 
@@ -68,13 +94,22 @@ export function handleLogCancelAutomation(event: LogCancelledAutomation): void {
   log.info("ID: {}", [dsaId]);
 
   let dsa = createOrLoadDsa(dsaId);
+  dsa.user = event.params.user;
   let cancelData = createOrLoadCancelData(eventId);
 
   cancelData.user = event.params.user;
   cancelData.userId = event.params.id;
   cancelData.nonce = event.params.nonce;
   cancelData.account = dsaId;
-
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  cancelData.transactionDetail = transaction.id;
+  
+  transaction.save();
   cancelData.save();
   dsa.save();
 }
@@ -82,7 +117,8 @@ export function handleLogCancelAutomation(event: LogCancelledAutomation): void {
 export function handleSystemCancelledAutomation(
   event: LogSystemCancelledAutomation
 ): void {
-  let dsaId = event.params.user.toHexString() + "#" + event.params.id.toString();
+  let dsaId =
+    event.params.user.toHexString() + "#" + event.params.id.toString();
   let eventId =
     event.transaction.hash.toHexString() + event.logIndex.toString();
 
@@ -93,6 +129,7 @@ export function handleSystemCancelledAutomation(
   log.info("ID: {}", [dsaId]);
 
   let dsa = createOrLoadDsa(dsaId);
+  dsa.user = event.params.user;
   let cancelData = createOrLoadSystemCancelData(eventId);
 
   cancelData.user = event.params.user;
@@ -100,15 +137,24 @@ export function handleSystemCancelledAutomation(
   cancelData.nonce = event.params.nonce;
   cancelData.errorCode = event.params.errorCode;
   cancelData.account = dsaId;
-
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  cancelData.transactionDetail = transaction.id;
+  
+  transaction.save();
   cancelData.save();
   dsa.save();
 }
 
-export function handleLogExecuteAutomationMetadata(
+export function handleLogExecuteAutomation(
   event: LogExecutedAutomation
 ): void {
-  let dsaId = event.params.user.toHexString() + "#" + event.params.id.toString();
+  let dsaId =
+    event.params.user.toHexString() + "#" + event.params.id.toString();
   let eventId =
     event.transaction.hash.toHexString() + event.logIndex.toString();
 
@@ -119,34 +165,7 @@ export function handleLogExecuteAutomationMetadata(
   log.info("ID: {}", [dsaId]);
 
   let dsa = createOrLoadDsa(dsaId);
-  let executeData = createOrLoadExecuteMetaData(eventId);
-
-  executeData.user = event.params.user;
-  executeData.userId = event.params.id;
-  executeData.nonce = event.params.nonce;
-  executeData.isSafe = event.params.isSafe;
-  executeData.metadata = event.params.metadata;
-  executeData.account = dsaId;
-
-  executeData.save();
-  dsa.save();
-}
-
-export function handleLogExecuteAutomation(
-  event: LogExecutedAutomationParams
-): void {
-  
-  let dsaId = event.params.user.toHexString() + "#" + event.params.id.toString();
-  let eventId =
-    event.transaction.hash.toHexString() + event.logIndex.toString();
-
-  log.info("transaction hash: {} and from: {} ", [
-    event.transaction.hash.toHexString(),
-    event.transaction.from.toHexString(),
-  ]);
-  log.info("ID: {}", [dsaId]);
-
-  let dsa = createOrLoadDsa(dsaId);
+  dsa.user = event.params.user;
   let executeData = createOrLoadExecute(eventId);
   let params = createOrLoadExecutionParams(eventId);
 
@@ -156,50 +175,183 @@ export function handleLogExecuteAutomation(
   executeData.finalHf = event.params.finalHf;
   executeData.initialHf = event.params.initialHf;
   executeData.automationFee = event.params.automationFee;
-  params.collateralToken = event.params.params.collateralToken; 
-  params.debtToken = event.params.params.debtToken; 
-  params.collateralAmount = event.params.params.collateralAmount; 
-  params.debtAmount = event.params.params.debtAmount; 
-  params.collateralAmountWithTotalFee = event.params.params.collateralAmountWithTotalFee; 
+  params.collateralToken = event.params.params.collateralToken;
+  params.debtToken = event.params.params.debtToken;
+  params.collateralAmount = event.params.params.collateralAmount;
+  params.debtAmount = event.params.params.debtAmount;
+  params.collateralAmountWithTotalFee =
+    event.params.params.collateralAmountWithTotalFee;
+  executeData.isSafe = event.params.isSafe;
+  executeData.metaData = event.params.metadata;
   let swaps = createOrLoadSwap(eventId);
   swaps.buyToken = event.params.params.swap.buyToken;
   swaps.sellToken = event.params.params.swap.sellToken;
   swaps.sellAmt = event.params.params.swap.sellAmt;
   swaps.unitAmt = event.params.params.swap.unitAmt;
   swaps.callData = event.params.params.swap.callData;
-  let spells = createOrLoadSpell(eventId);
-  spells._datas = event.params.spells._datas;
-  spells._targets = event.params.spells._targets;
-  params.swap = swaps.id; 
+  params.swap = swaps.id;
   executeData.params = params.id;
-  executeData.spells = spells.id;
   executeData.account = dsaId;
-
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  executeData.transactionDetail = transaction.id;
+  
+  transaction.save();
   params.save();
   swaps.save();
-  spells.save();
   executeData.save();
   dsa.save();
 }
 
+export function handleLogChangedOwner(event: LogChangedOwner): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let changeOwnerData = createOrLoadChangedOwner(eventId);
+  changeOwnerData.oldOwner = event.params.oldOnwer;
+  changeOwnerData.newOwner = event.params.newOnwer;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  changeOwnerData.transactionDetail = transaction.id;
+  
+  transaction.save();
+  changeOwnerData.save();
+} 
+
+export function handleLogFeeTransferred(event: LogFeeTransferred): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let data = createOrLoadFeeTransferData(eventId);
+  data.recipient = event.params.recipient;
+  let tokens = event.params.tokens;
+  let amounts = event.params.amount;
+  data.from = event.transaction.from;
+  let dataTokens = data.tokens;
+  let dataAmounts = data.amount;
+
+  for(let i = 0; i<tokens.length; i++) {
+    dataTokens.push(tokens[i]);
+    dataAmounts.push(amounts[i]);
+  }
+
+  data.tokens = dataTokens;
+  data.amount = dataAmounts;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  data.transactionDetail = transaction.id;
+  
+  transaction.save();
+  data.save();
+} 
+
+export function handleLogSystemCall(event: LogSystemCall): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let data = createOrLoadSystemCallData(eventId);
+  data.sender = event.params.sender;
+  data.actionId = event.params.actionId;
+  data.metaData = event.params.metadata;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  data.transactionDetail = transaction.id;
+  
+  transaction.save();
+  data.save();
+} 
+
+export function handleLogUpdateAutomationFee(event: LogUpdatedAutomationFee): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let data = createOrUpdateAutomationFeeData(eventId);
+  data.oldAutomationFee = event.params.oldAutomationFee;
+  data.newAutomationFee = event.params.newAutomationFee;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  data.transactionDetail = transaction.id;
+  
+  transaction.save();
+  data.save();
+} 
+
+export function handleUpdatedBufferHf(event: LogUpdatedBufferHf): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let data = createOrUpdateBufferHfData(eventId);
+  data.oldBuffer = event.params.oldBufferHf;
+  data.newBuffer = event.params.newBufferHf;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  data.transactionDetail = transaction.id;
+  
+  transaction.save();
+  data.save();
+}
+
+export function handleLogUpdatedMinHf(event: LogUpdatedMinHf): void {
+  let eventId =
+    event.transaction.hash.toHexString() + event.logIndex.toString();
+
+  let data = createOrUpdateMinHfData(eventId);
+  data.oldMinHf = event.params.oldMinHf;
+  data.newMinHf = event.params.newMinHf;
+  let transaction = createOrLoadTransaction(eventId);
+  transaction.blockNumber = event.block.number;
+  transaction.timeStamp = event.block.timestamp;
+  transaction.logIndex = event.logIndex;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.transactionLogIndex = event.transactionLogIndex;
+  data.transactionDetail = transaction.id;
+  
+  transaction.save();
+  data.save();
+}
+
 export function handleExecutors(event: LogFlippedExecutors): void {
-  let id = "ALL"
+  let id = "ALL";
   let executors_ = Executor.load(id);
-  if(executors_ == null){
+  if (executors_ == null) {
     executors_ = new Executor(id);
     executors_.executors = [];
   }
   let execArr = executors_.executors;
   let statusArr = event.params.status;
-  for(let i=0;i<event.params.executors.length;i++){
+  for (let i = 0; i < event.params.executors.length; i++) {
     let index = execArr.indexOf(event.params.executors[i]);
-    if(statusArr[i] === true){
-      if(index == -1){
+    if (statusArr[i] === true) {
+      if (index == -1) {
         execArr.push(event.params.executors[i]);
       }
     } else {
-      if(index != -1){
-        execArr.splice(index-1, 1);
+      if (index != -1) {
+        execArr.splice(index - 1, 1);
       }
     }
   }
--- src/insta-index.ts
+++ src/insta-index.ts
@@ -4,17 +4,8 @@ import {
   Bytes,
   DataSourceContext,
 } from "@graphprotocol/graph-ts";
-import {
-  InstaIndex,
-  LogAccountCreated,
-  LogNewAccount,
-  LogNewCheck,
-  LogNewMaster,
-  LogUpdateMaster,
-} from "../generated/InstaIndex/InstaIndex";
-import { Account, ExecutionParams, SubmitData, Swap, Spell, ExecuteData, ExecuteMetaData, CancelData, SystemCancelData } from "../generated/schema";
-import { InstaAutomation } from "../generated/InstaIndex/InstaAutomation";
-import { InstaList } from "../generated/InstaIndex/InstaList";
+import { Account, ExecutionParams, SubmitData, Swap, Spell, ExecuteData, CancelData, SystemCancelData, TransactionData, ChangedOwner, FeeTransferData, SystemCallData, UpdateAutomationFeeData, UpdateBufferHfData, UpdateMinHfData } from "../generated/schema";
+import { InstaAutomation } from "../generated/InstaAutomation/InstaAutomation";
 import { InstaAutomation as InstaAutomationABI } from "../generated/templates";
 
 export const ZERO = new BigInt(0);
@@ -25,6 +16,7 @@ export function createOrLoadDsa(id: string): Account {
   let account = Account.load(id);
   if (account == null) {
     account = new Account(id);
+    account.user = ADDR_ZERO;
   }
   return account;
 }
@@ -38,8 +30,33 @@ export function createOrLoadSubmit(id: string): SubmitData {
         submitData.safeHF = ZERO;
         submitData.thresholdHF = ZERO;
         submitData.currentHf = ZERO;
+        submitData.transactionDetail = createOrLoadTransaction(id).id;
     }
     return submitData;
+}
+
+export function createOrLoadTransaction(id: string): TransactionData {
+  let transactionData = TransactionData.load(id);
+  if(transactionData == null) {
+      transactionData = new TransactionData(id);
+      transactionData.blockNumber = ZERO;
+      transactionData.timeStamp = ZERO;
+      transactionData.logIndex = ZERO;
+      transactionData.transactionHash = ADDR_ZERO;
+      transactionData.transactionLogIndex = ZERO; 
+  }
+  return transactionData;
+}
+
+export function createOrLoadChangedOwner(id: string): ChangedOwner {
+  let ownerData = ChangedOwner.load(id);
+  if(ownerData == null) {
+      ownerData = new ChangedOwner(id);
+      ownerData.oldOwner = ADDR_ZERO;
+      ownerData.newOwner = ADDR_ZERO; 
+      ownerData.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return ownerData;
 }
 
 export function createOrLoadExecutionParams(id: string): ExecutionParams {
@@ -72,16 +89,6 @@ export function createOrLoadSwap(id: string): Swap {
   return swap;
 }
 
-export function createOrLoadSpell(id: string): Spell {
-  let spell = Spell.load(id);
-  if(spell == null){
-    spell = new Spell(id);
-    spell._datas = [];
-    spell._targets = [];
-  }
-  return spell;
-}
-
 export function createOrLoadExecute(id: string): ExecuteData {
   let data = ExecuteData.load(id);
   if(data == null) {
@@ -93,20 +100,9 @@ export function createOrLoadExecute(id: string): ExecuteData {
     data.initialHf = ZERO;
     data.automationFee = 0;
     data.params = createOrLoadExecutionParams(id).id;
-    data.spells = createOrLoadSpell(id).id;
-  }
-  return data;
-}
-
-export function createOrLoadExecuteMetaData(id: string): ExecuteMetaData {
-  let data = ExecuteMetaData.load(id);
-  if(data == null) {
-    data = new ExecuteMetaData(id);
-    data.user = ADDR_ZERO;
-    data.userId = ZERO;
-    data.nonce = ZERO;
+    data.transactionDetail = createOrLoadTransaction(id).id;
     data.isSafe = false;
-    data.metadata = new Bytes(0);
+    data.metaData = new Bytes(0);
   }
   return data;
 }
@@ -118,6 +114,7 @@ export function createOrLoadCancelData(id: string): CancelData {
     data.user = ADDR_ZERO;
     data.userId = ZERO;
     data.nonce = ZERO;
+    data.transactionDetail = createOrLoadTransaction(id).id;
   }
   return data;
 }
@@ -131,6 +128,65 @@ export function createOrLoadSystemCancelData(id: string): SystemCancelData {
     data.userId = ZERO;
     data.nonce = ZERO;
     data.errorCode = 0;
+    data.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return data;
+}
+
+export function createOrLoadFeeTransferData(id: string): FeeTransferData {
+  let data = FeeTransferData.load(id);
+  if(data == null) {
+    data = new FeeTransferData(id);
+    data.recipient = ADDR_ZERO;
+    data.from = ADDR_ZERO;
+    data.tokens = [];
+    data.amount = [];
+    data.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return data;
+}
+
+export function createOrLoadSystemCallData(id: string): SystemCallData {
+  let data = SystemCallData.load(id);
+  if(data == null) {
+    data = new SystemCallData(id);
+    data.sender = ADDR_ZERO;
+    data.actionId = "";
+    data.metaData = new Bytes(0);
+    data.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return data;
+}
+
+export function createOrUpdateAutomationFeeData(id: string): UpdateAutomationFeeData {
+  let data = UpdateAutomationFeeData.load(id);
+  if(data == null) {
+    data = new UpdateAutomationFeeData(id);
+    data.oldAutomationFee = 0;
+    data.newAutomationFee = 0;
+    data.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return data;
+}
+
+export function createOrUpdateBufferHfData(id: string): UpdateBufferHfData {
+  let data = UpdateBufferHfData.load(id);
+  if(data == null) {
+    data = new UpdateBufferHfData(id);
+    data.oldBuffer = ZERO;
+    data.newBuffer = ZERO;
+    data.transactionDetail = createOrLoadTransaction(id).id;
+  }
+  return data;
+}
+
+export function createOrUpdateMinHfData(id: string): UpdateMinHfData {
+  let data = UpdateMinHfData.load(id);
+  if(data == null) {
+    data = new UpdateMinHfData(id);
+    data.oldMinHf = ZERO;
+    data.newMinHf = ZERO;
+    data.transactionDetail = createOrLoadTransaction(id).id;
   }
   return data;
 }
